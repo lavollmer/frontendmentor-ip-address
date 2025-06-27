38ac9e1fdbadfb4a8da38ffa7dcee7a53f6a87ba
Laura <laura.e.vollmer@gmail.com>
2025-06-27 07:41:15 -0500

    Added Google Fonts and updated Title of webpae

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Search from "./components/searchbar"
+import IpAddress from "./components/ipaddress"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1>Hello</h1>
+        <Search />
+        <IpAddress />
       </div>
-
     </>
   )
 }
---
+++ src/components/searchbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function searchbar () {
+  return (
+    <div>searchbar</div>
+  )
+}
+
+export default searchbar
