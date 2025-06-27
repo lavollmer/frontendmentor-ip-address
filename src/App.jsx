986c290b35d2e85f5ae0b78155e8bf8b80ff922e
Laura <laura.e.vollmer@gmail.com>
2025-06-27 07:58:55 -0500

    Updating the frontpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import Search from "./components/searchbar"
 import IpAddress from "./components/ipaddress"
-import DesktopBackground from "./assets/images/pattern-bg-desktop.png"
-import MobileBackground from "./assets/images/pattern-bg-mobile.png"
+import DesktopBackground from "./assets/pattern-bg-desktop.png"
+import MobileBackground from "./assets/pattern-bg-mobile.png"
 
 function App() {
   return (
     <>
       <div>
-        <img src={DesktopBackground} alt="Desktop Background" />
-        <img src={MobileBackground} alt="Mobile Background"/>
+        <div className='position-relative text-center' style={{ height: '300px' }}>
+          <img src={DesktopBackground} alt="Desktop Background" className='position-absolute top-0 right-0 left-0 w-100' />
+          <h1 className='position-absolute w-100' style={{
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'white',
+          }}>IP Address Tracker</h1>
+        </div>
         <Search />
         <IpAddress />
       </div>
