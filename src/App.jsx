c035ba9e30332a058cf80c5e81c289bd1b119879
Laura <laura.e.vollmer@gmail.com>
2025-07-07 14:25:04 -0500

    Created a separate div container with CSS for the ipaddress to sit on top of the map and background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Ipaddress from "./components/ipaddress"
 import Header from "./components/header"
 
 function App() {
-    return (
-      <>
-        <div class='desktop-background'>
-          <Header />
+  return (
+    <>
+      <div class='desktop-background'>
+        <Header />
+        <div className='desktop-ip'>
           <Ipaddress />
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default App
