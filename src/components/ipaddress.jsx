53fdb70ba2141a072e49527490ae49ada1958e52
Laura <laura.e.vollmer@gmail.com>
2025-07-07 14:22:02 -0500

    Created the grey line

--- src/components/ipaddress.jsx
+++ src/components/ipaddress.jsx
@@ -7,14 +7,17 @@ function ipaddress() {
                 <h1>IP ADDRESS</h1>
                 <h2>COORDINATES</h2>
             </div>
+            <div className='line'></div>
             <div className='search-box'>
                 <h1>LOCATION</h1>
                 <h2>COORDINATES</h2>
             </div>
+            <div className='line'></div>
             <div className='search-box'>
                 <h1>TIMEZONE</h1>
                 <h2>COORDINATES</h2>
             </div>
+            <div className='line'></div>
             <div className='search-box'>
                 <h1>ISP</h1>
                 <h2>COORDINATES</h2>
