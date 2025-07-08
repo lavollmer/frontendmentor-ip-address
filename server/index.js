27ce1688fff8aaae40d43647d2b26e62f6e71b47
Laura <laura.e.vollmer@gmail.com>
2025-07-08 10:23:49 -0500

    Adding index.js to backend

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ const express = require ('express');
 const cors = require('cors');
 require('dotenv').config()
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req,res)=> {
+    res.json({message: 'Hello from the backend!'})
+
+})
+
+app.get('/api/data', async(req, res) => {
+    const apiKey = process.env.API_KEY;
+    res.json({apiKey});
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:${PORT}')
+})
