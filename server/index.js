c72e242a99bca7a04b0db81ebf3ece9bd7e654f2
Laura <laura.e.vollmer@gmail.com>
2025-07-08 13:17:50 -0500

    Debugged the env Code KEY API

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config()
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req,res)=> {
-    res.json({message: 'Hello from the backend!'})
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from the backend!' })
 
 })
 
-app.get('/api/data', async(req, res) => {
+app.get('/api/data', async (req, res) => {
     const searchTerm = req.query.query;
     const apiKey = process.env.VITE_REACT_APP_KEY;
 
@@ -22,8 +22,8 @@ app.get('/api/data', async(req, res) => {
         const response = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${searchTerm}`)
         res.json(response.data);
     } catch {
-        console.error('Error fetching data.')
-        res.status(500).json({error: "Failed to fetch location data."})
+        console.error('Error fetching from GeoIP API:', error.message);
+        res.status(500).json({ error: 'Failed to fetch location data' });
     }
 })
 
