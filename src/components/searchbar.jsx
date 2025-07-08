ad0fffad121b09bde9ba96d4e29d3c4cd4ebe1c8
Laura <laura.e.vollmer@gmail.com>
2025-07-08 09:40:34 -0500

    Added useEffect in searchbar

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Arrow from "../assets/icon-arrow.svg"
 import '../header.css'
 
@@ -11,6 +11,39 @@ function searchbar() {
         setSearchTerm(event.target.value);
         onSearch(event.target.value);
     }
+
+      // Manage the state of location data, error, and loading
+  const [locationData, setLocationData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // use effect to run fetchLocation when component mounts, empty dependency array runs once
+  useEffect(() => {
+    const fetchLocation = async ({searchTerm}) => {
+      setLoading(true);
+      try {
+        // get request through axios
+        // process.env.GEOLOCATION_API
+        const response = await axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_gVO7K7aIuD2FB8QyhWeZ5CiqXc77X&ipAddress=8.8.8.8')
+        if (!response.ok){
+          throw new Error ('Network response was not ok.')
+        }
+        const result = await response.json();
+        setLocationData(result);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchLocation();
+  }, [searchTerm]);
+
+  if (error) {
+    return <div>{error.message}</div>
+  }
+
+  if (loading) return <div>Loading ...</div>;
 
     return (
         <div className="search-container">
