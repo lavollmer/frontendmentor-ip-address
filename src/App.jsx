df12273bd639f9f1b96471dd09ef35485f4b6f18
Laura <laura.e.vollmer@gmail.com>
2025-07-09 11:09:19 -0500

    Updating mobile responsive features

--- src/App.jsx
+++ src/App.jsx
@@ -45,66 +45,68 @@ function App() {
 
   return (
     <>
-      <div className='container-desktop'>
-        <div className="header-container">
-          <img src={DesktopImage} alt="Desktop Background" className='desktop-background' />
-          <div className='header-text'>
-            <h1 className='title-container font-bold'>IP Address Tracker</h1>
-            <div className="search-container">
-              <input type="text" name="search" value={searchTerm} onChange={handleChange} placeholder="Search for an IP address or domain" />
-              <button className="search-button" onClick={handleSearch}>
-                <img src={Arrow} alt="Search Icon" />
-              </button>
+      <div className='container'>
+        <div className='container-desktop'>
+          <div className="header-container">
+            <img src={DesktopImage} alt="Desktop Background" className='desktop-background' />
+            <div className='header-text'>
+              <h1 className='title-container font-bold'>IP Address Tracker</h1>
+              <div className="search-container">
+                <input type="text" name="search" value={searchTerm} onChange={handleChange} placeholder="Search for an IP address or domain" />
+                <button className="search-button" onClick={handleSearch}>
+                  <img src={Arrow} alt="Search Icon" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {error && <div>{error.message}</div>}
-      {loading ? (
-        <div>Loading ... </div>
-      ) : (
-        <div className='desktop-ip-display'>
-          <div className='ip-search'>
-            <div className='search-box'>
-              <h1>IP ADDRESS</h1>
-              <p>{locationData?.ip ?? 'Loading ..'}</p>
+        {error && <div>{error.message}</div>}
+        {loading ? (
+          <div>Loading ... </div>
+        ) : (
+          <div className='desktop-ip-display'>
+            <div className='ip-search'>
+              <div className='search-box'>
+                <h1>IP ADDRESS</h1>
+                <p>{locationData?.ip ?? 'Loading ..'}</p>
+              </div>
+              <div className='line-decorate'></div>
+              <div className='search-box'>
+                <h1>LOCATION</h1>
+                <p>{locationData?.location?.city ?? 'Loading ..'}</p>
+              </div>
+              <div className='line-decorate'></div>
+              <div className='search-box'>
+                <h1>TIMEZONE</h1>
+                <p>{locationData?.location?.timezone ?? 'Loading...'}</p>
+              </div>
+              <div className='line-decorate'></div>
+              <div className='search-box'>
+                <h1>ISP</h1>
+                <p>{locationData?.isp ?? 'Loading..'}</p>
+              </div>
             </div>
-            <div className='line-decorate'></div>
-            <div className='search-box'>
-              <h1>LOCATION</h1>
-              <p>{locationData?.location?.city ?? 'Loading ..'}</p>
-            </div>
-            <div className='line-decorate'></div>
-            <div className='search-box'>
-              <h1>TIMEZONE</h1>
-              <p>{locationData?.location?.timezone ?? 'Loading...'}</p>
-            </div>
-            <div className='line-decorate'></div>
-            <div className='search-box'>
-              <h1>ISP</h1>
-              <p>{locationData?.isp ?? 'Loading..'}</p>
+            {/* Leaflet Map */}
+            <div className='leaflet-container'>
+              {/* Map Container taking center coordinates and zoom level as props */}
+              <MapContainer center={position} zoom={13}>
+                {/* Tile layer specifies the tile server */}
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {/* marker adds specific location */}
+                <Marker position={position} icon={Icon}>
+                </Marker>
+              </MapContainer>
             </div>
           </div>
-          {/* Leaflet Map */}
-          <div className='leaflet-container'>
-            {/* Map Container taking center coordinates and zoom level as props */}
-            <MapContainer center={position} zoom={13}>
-              {/* Tile layer specifies the tile server */}
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {/* marker adds specific location */}
-              <Marker position={position} icon={Icon}>
-              </Marker>
-            </MapContainer>
-          </div>
+        )
+        }
+        <div className='footer'>
+          <p>
+            Created by Laura Dev | 2025.</p>
         </div>
-      )
-      }
-      <div className='footer'>
-        <p>
-          Created by Laura Dev | 2025.</p>
       </div>
     </>
   )
